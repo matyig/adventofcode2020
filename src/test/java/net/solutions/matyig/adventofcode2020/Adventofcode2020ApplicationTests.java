72a6521583fd28399e5e570c78401caa98a095a7
Gabor Matyi <gabor.matyi@gmail.com>
2022-11-24 18:13:41 +0100

    initial commit

---
+++ src/test/java/net/solutions/matyig/adventofcode2020/Adventofcode2020ApplicationTests.java
@@ -0,0 +1,13 @@
+package net.solutions.matyig.adventofcode2020;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Adventofcode2020ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
